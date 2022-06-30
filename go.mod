239518d192edcca425bdf4229f64a6f6783a95bf
Kurt McAlpine <kurt@linux.com>
2022-06-30 14:24:19 +1200

    Tidy up

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/kurtmc/github-action-python-versioner
 go 1.18
 
 require (
-	github.com/bigkevmcd/go-configparser v0.0.0-20210106142102-909504547ead
 	github.com/blang/semver/v4 v4.0.0
 	github.com/go-ini/ini v1.66.6
+	github.com/pelletier/go-toml v1.9.5
 )
+
+require github.com/stretchr/testify v1.8.0 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/blang/semver/v4"
-	"github.com/go-ini/ini"
+	"github.com/pelletier/go-toml"
 	"os"
 	"os/exec"
 	"strings"
@@ -51,16 +51,12 @@ func getGitTagVersion() (string, error) {
 }
 
 func getSetupCfgVersion() (string, error) {
-	setupCfg, err := ini.Load("setup.cfg")
+	config, err := toml.LoadFile("pyproject.toml")
 	if err != nil {
 		return "", err
 	}
-	metadata, err := setupCfg.GetSection("metadata")
-	if err != nil {
-		return "", err
-	}
-	version, err := metadata.GetKey("version")
-	return version.String(), err
+	project := config.Get("project")
+	return project.(*toml.Tree).Get("version").(string), nil
 }
 
 func configureGit() error {
@@ -129,27 +125,26 @@ func updateTagAndSetupCfg(newVersion string) error {
 	if err != nil {
 		return err
 	}
-	setupCfg, err := ini.Load("setup.cfg")
+
+	config, err := toml.LoadFile("pyproject.toml")
 	if err != nil {
 		return err
 	}
-	metadata, err := setupCfg.GetSection("metadata")
-	if err != nil {
-		return err
-	}
-	version, err := metadata.GetKey("version")
-	if err != nil {
-		return err
-	}
-	if version.String() != newVersion {
-		metadata.NewKey("version", newVersion)
-		setupCfg.SaveTo("setup.cfg")
-		cmd := exec.Command("git", "add", "setup.cfg")
-		_, err := cmd.Output()
+	project := config.Get("project")
+	version := project.(*toml.Tree).Get("version").(string)
+
+	if version != newVersion {
+		project.(*toml.Tree).Set("version", newVersion)
+		err := writeToml("pyproject.toml", config)
 		if err != nil {
 			return err
 		}
-		cmd = exec.Command("git", "commit", "-m", fmt.Sprintf("update version to %s in setup.cfg", newVersion))
+		cmd := exec.Command("git", "add", "pyproject.toml")
+		_, err = cmd.Output()
+		if err != nil {
+			return err
+		}
+		cmd = exec.Command("git", "commit", "-m", fmt.Sprintf("update version to %s in pyproject.toml", newVersion))
 		_, err = cmd.Output()
 		if err != nil {
 			return err
@@ -168,4 +163,12 @@ func updateTagAndSetupCfg(newVersion string) error {
 		return err
 	}
 	return nil
+}
+
+func writeToml(path string, config *toml.Tree) error {
+	t, err := toml.Marshal(config)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, t, 0644)
 }
